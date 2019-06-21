740dddea7c6d62f41b23b1c77ed238f3ffe9f38c
Rafael-Lopez <rafael.lopez@bbd.ca>
2019-06-21 14:54:57 -0400

    Preventing duplicates with @Column(unique=true)

--- src/main/java/lopez/rafael/mobileappws/data/entities/User.java
+++ src/main/java/lopez/rafael/mobileappws/data/entities/User.java
@@ -18,7 +18,7 @@ public class User implements Serializable {
     private String firstName;
     @Column(nullable = false, length = 50)
     private String lastName;
-    @Column(nullable = false, length = 120)
+    @Column(nullable = false, length = 120, unique = true)
     private String email;
     @Column(nullable = false)
     private String encryptedPassword;
