496bbab65826ad3d0ad933716a55e79585c96de8
Rafael-Lopez <rafael.lopez@bbd.ca>
2019-06-22 12:42:27 -0400

    Customizing User Login URL

--- src/main/java/lopez/rafael/mobileappws/security/WebSecurity.java
+++ src/main/java/lopez/rafael/mobileappws/security/WebSecurity.java
@@ -22,11 +22,20 @@ public class WebSecurity extends WebSecurityConfigurerAdapter {
     public void configure(HttpSecurity http) throws Exception{
         http.csrf().disable().authorizeRequests()
                 .mvcMatchers(HttpMethod.POST, SecurityConstants.SIGN_UP_URL)
-                .permitAll().anyRequest().authenticated().and().addFilter(new AuthenticationFilter(authenticationManager()));
+                .permitAll().anyRequest().authenticated().and().addFilter(getAuthenticationFilter());
+                //Replacing this line to customize login URL
+                //.permitAll().anyRequest().authenticated().and().addFilter(new AuthenticationFilter(authenticationManager()));
     }
 
     @Override
     public void configure(AuthenticationManagerBuilder auth) throws Exception{
         auth.userDetailsService(userService).passwordEncoder(bCryptPasswordEncoder);
+    }
+
+    //This is just to customize login URL. Not really needed.
+    public AuthenticationFilter getAuthenticationFilter() throws Exception{
+        final AuthenticationFilter filter = new AuthenticationFilter(authenticationManager());
+        filter.setFilterProcessesUrl("/users/login");
+        return filter;
     }
 }
