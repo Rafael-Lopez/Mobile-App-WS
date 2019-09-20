714ed66054d39fda6043c646bc437da77d72451a
Rafael-Lopez <rafael.lopez@bbd.ca>
2019-09-20 15:02:22 -0400

    Adding Address entity

--- src/main/java/lopez/rafael/mobileappws/data/entities/Address.java
+++ src/main/java/lopez/rafael/mobileappws/data/entities/Address.java
@@ -1,13 +1,31 @@
-package lopez.rafael.mobileappws.models.dtos;
+package lopez.rafael.mobileappws.data.entities;
 
-public class AddressDto {
+import lopez.rafael.mobileappws.models.dtos.UserDto;
+
+import javax.persistence.*;
+import java.io.Serializable;
+
+@Entity
+public class Address implements Serializable {
+    private static final long serialVersionUID = 3865903039190150623L;
+
+    @Id
+    @GeneratedValue
     private long id;
+    @Column(length = 30, nullable = false)
     private String addressId;
+    @Column(length = 15, nullable = false)
     private String city;
+    @Column(length = 15, nullable = false)
     private String country;
+    @Column(length = 100, nullable = false)
     private String streetName;
+    @Column(length = 7, nullable = false)
     private String postalCode;
+    @Column(length = 10, nullable = false)
     private String type;
+    @ManyToOne
+    @JoinColumn(name = "user_id")
     private UserDto userDetails;
 
     public long getId() {
@@ -16,6 +34,14 @@ public class AddressDto {
 
     public void setId(long id) {
         this.id = id;
+    }
+
+    public String getAddressId() {
+        return addressId;
+    }
+
+    public void setAddressId(String addressId) {
+        this.addressId = addressId;
     }
 
     public String getCity() {
@@ -64,13 +90,5 @@ public class AddressDto {
 
     public void setUserDetails(UserDto userDetails) {
         this.userDetails = userDetails;
-    }
-
-    public String getAddressId() {
-        return addressId;
-    }
-
-    public void setAddressId(String addressId) {
-        this.addressId = addressId;
     }
 }
