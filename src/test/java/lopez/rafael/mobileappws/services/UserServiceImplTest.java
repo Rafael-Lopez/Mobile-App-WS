dfcd2b3548a10dd684881f290e052f4e68a46a41
Rafael-Lopez <rafael.lopez@bbd.ca>
2019-09-23 16:17:11 -0400

    Adding JUnit 5 to classpath + Adding first test

---
+++ src/test/java/lopez/rafael/mobileappws/services/UserServiceImplTest.java
@@ -0,0 +1,18 @@
+package lopez.rafael.mobileappws.services;
+
+import org.junit.Test;
+import org.junit.jupiter.api.BeforeEach;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class UserServiceImplTest {
+
+    @BeforeEach
+    void setUp() throws Exception {
+
+    }
+
+    @Test
+    public final void testGetUser() {
+
+    }
+}
